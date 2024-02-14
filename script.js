7a1c5a58efbfd6e04038dfc3ad567d83794041b6
TwanAsselbergs <twanasselbergs@outlook.com>
2024-02-14 10:41:52 +0100

    p

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+// Haal de slider op met behulp van zijn ID priceSlider
 let slider = document.getElementById("priceSlider");
+
+// Haal het output-element op met behulp van zijn ID sliderValue
 let output = document.getElementById("sliderValue");
+
+// Stel de waarde van de output in op de huidige waarde van de slider
 output.innerHTML = slider.value;
 
+// Wanneer de slider verandert, update dan de waarde van de output
 slider.oninput = function () {
   output.innerHTML = this.value;
 };
